c30546a1e6e4550092546941e2e5379bf06ee3cd
Daria Boiprav <boipravd19@gmail.com>
2019-04-25 17:37:50 +0300

    DAASHA TEST

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static java.lang.Thread.sleep;
 
